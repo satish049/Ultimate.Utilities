0981dbd0df5b0d6735c7739ded50409cae7ed760
sbonumaddi <sbonumaddi@mirthcorp.com>
2016-11-18 17:05:04 +0530

    AssemblyInfo changes

--- Ultimate.Utilities/Properties/AssemblyInfo.cs
+++ Ultimate.Utilities/Properties/AssemblyInfo.cs
@@ -5,9 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Ultimate.Utilities")]
+[assembly: AssemblyDescription("Ultimate.Utilities provides the common utility functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SatishKumarB")]
 [assembly: AssemblyProduct("Ultimate.Utilities")]
+[assembly: AssemblyCopyright("Copyright © Satish Kumar B 2016")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
